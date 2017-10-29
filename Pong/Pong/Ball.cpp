39954d821c328eab61997f5134f47ec6041876c5
jameszolyak <jameszolyak@gmail.com>
2017-10-29 12:32:15 -0700

    Ball now properly collides and bounces off of upper and lower walls as well as the player and enemy paddles.

--- Pong/Pong/Ball.cpp
+++ Pong/Pong/Ball.cpp
@@ -7,7 +7,7 @@ Ball::Ball(float f)
 	radius = f;
 	ball.setRadius(radius - 3);
 	ball.setOutlineThickness(3);
-	ball.setOutlineColor(Color::Black);
+	ball.setOutlineColor(Color::White);
 	ball.setFillColor(Color::White);
 	ball.setOrigin(radius / 2, radius / 2);
 }
--- Pong/Pong/Source.cpp
+++ Pong/Pong/Source.cpp
@@ -18,15 +18,16 @@ int main()
 	const int gameHeight = 600;
 	RenderWindow window(VideoMode(gameWidth, gameHeight), "Pong");
 	Paddle* player = new Paddle(Vector2f(25, 100));
-	player->position = Vector2f(20, 60);
+	//player->position = Vector2f(20, 60);
 	player->speed = 400;
 
 	Paddle* enemy = new Paddle(Vector2f(25, 100));
-	enemy->position = Vector2f(400, 100);
+	//enemy->position = Vector2f(400, 100);
 	enemy->speed = 400;
 
 	Ball* ball = new Ball(10.0f);
-	ball->position = Vector2f(200, 200);
+	//ball->position = Vector2f(200, 200);
+	ball->speed = 100.0f;
 	bool isPlaying = false;
 	Clock clock;
 	Clock AITimer;
@@ -37,7 +38,13 @@ int main()
 		{
 			player->paddle.setPosition(10 + player->dimensions.x / 2, gameHeight / 2);
 			enemy->paddle.setPosition(gameWidth - 10 - enemy->dimensions.x / 2, gameHeight / 2);
-			ball->ball.setPosition(gameWidth / 2, gameHeight / 2);
+			//ball->ball.setPosition(gameWidth / 2, gameHeight / 2);
+			ball->ball.setPosition(400, 200);
+			do
+			{
+				// Make sure the ball initial angle is not too much vertical
+				ball->ballAngle = (std::rand() % 360) * 2 * pi / 360;
+			} while (std::abs(std::cos(ball->ballAngle)) < 0.7f);
 		}
 		isPlaying = true;
 		Event event;
@@ -56,28 +63,28 @@ int main()
 		{
 			float deltaTime = clock.restart().asSeconds();
 			if (Keyboard::isKeyPressed(Keyboard::Up) &&
-				(player->position.y - player->dimensions.y / 2 > 5.f))
+				(player->paddle.getPosition().y - player->dimensions.y / 2 > 20.f))
 			{
 				player->paddle.move(0.f, -player->speed * deltaTime);
 			}
 			if (Keyboard::isKeyPressed(Keyboard::Down) &&
-				(player->position.y + player->dimensions.y / 2 < gameHeight - 5.f))
+				(player->paddle.getPosition().y + player->dimensions.y / 2 < gameHeight - 20.f))
 			{
 				player->paddle.move(0.f, player->speed * deltaTime);
 			}
 
-			if (((enemy->speed < 0.f) && (enemy->position.y - enemy->dimensions.y / 2 > 5.f)) ||
-				((enemy->speed > 0.f) && (enemy->position.y + enemy->dimensions.y / 2 < gameHeight - 5.f)))
+			if (((enemy->speed < 0.f) && (enemy->paddle.getPosition().y - enemy->dimensions.y / 2 > 20.f)) ||
+				((enemy->speed > 0.f) && (enemy->paddle.getPosition().y + enemy->dimensions.y / 2 < gameHeight - 20.f)))
 			{
 				enemy->paddle.move(0.f, enemy->speed * deltaTime);
 			}
-
+			
 			if (AITimer.getElapsedTime() > AITime)
 			{
 				AITimer.restart();
-				if (ball->position.y + ball->radius > enemy->position.y + enemy->dimensions.y / 2)
+				if (ball->ball.getPosition().y > enemy->paddle.getPosition().y )
 					enemy->speed = 400;
-				else if (ball->position.y - ball->radius < enemy->position.y - enemy->dimensions.y / 2)
+				else if (ball->ball.getPosition().y < enemy->paddle.getPosition().y  )
 					enemy->speed = -400;
 				else
 					enemy->speed = 0.f;
@@ -87,60 +94,83 @@ int main()
 			ball->ball.move(std::cos(ball->ballAngle) * factor, std::sin(ball->ballAngle) * factor);
 
 			// Check collisions between the ball and the screen
-			if (ball->position.x - ball->radius < 0.f)
+			if (ball->ball.getPosition().x - ball->radius < 0.f)
 			{
-				isPlaying = false;
+				//isPlaying = false;
 				//pauseMessage.setString("You lost!\nPress space to restart or\nescape to exit");
 			}
-			if (ball->position.x + ball->radius > gameWidth)
+			if (ball->ball.getPosition().x + ball->radius > gameWidth)
 			{
-				isPlaying = false;
+				//isPlaying = false;
 				//pauseMessage.setString("You won!\nPress space to restart or\nescape to exit");
 			}
-			if (ball->position.y - ball->radius < 0.f)
+			if (ball->ball.getPosition().y - ball->radius < 0.f)
 			{
 				//ballSound.play();
 				ball->ballAngle = -ball->ballAngle;
-				ball->ball.setPosition(ball->position.x, ball->radius + 0.1f);
-				//ball->position.x = ball->radius + 0.1f;
+				ball->ball.setPosition(ball->ball.getPosition().x, ball->radius + 0.1f);
 			}
-			if (ball->position.y + ball->radius > gameHeight)
+			if (ball->ball.getPosition().y + ball->radius > gameHeight)
 			{
 				//ballSound.play();
 				ball->ballAngle = -ball->ballAngle;
-				ball->ball.setPosition(ball->position.x, gameHeight - ball->radius - 0.1f);
+				ball->ball.setPosition(ball->ball.getPosition().x, gameHeight - ball->radius - 0.1f);
 			}
-
+			
+				
 			// Check the collisions between the ball and the paddles
 			// Left Paddle
-			if (ball->position.x - ball->radius < player->position.x + player->dimensions.x / 2 &&
-				ball->position.x - ball->radius > player->position.x &&
-				ball->position.y + ball->radius >= player->position.y - player->dimensions.y / 2 &&
-				ball->position.y - ball->radius <= player->position.y + player->dimensions.y / 2)
+
+			if (ball->ball.getGlobalBounds().intersects(player->paddle.getGlobalBounds()))
 			{
-				if (ball->position.y > player->position.y)
+				if (ball->ball.getOrigin().y > player->paddle.getOrigin().y)
+					ball->ballAngle = pi - ball->ballAngle + (std::rand() % 20) * pi / 180;
+				else
+					ball->ballAngle = pi - ball->ballAngle - (std::rand() % 20) * pi / 180;
+				
+				//ballSound.play();
+				//ball->ball.setPosition(player->paddle.getPosition().x + ball->radius + player->dimensions.x / 2 + 10.0f, ball->ball.getPosition().y);
+				
+			}
+
+			if (ball->ball.getGlobalBounds().intersects(enemy->paddle.getGlobalBounds()))
+			{
+				if (ball->ball.getOrigin().y > enemy->paddle.getOrigin().y)
 					ball->ballAngle = pi - ball->ballAngle + (std::rand() % 20) * pi / 180;
 				else
 					ball->ballAngle = pi - ball->ballAngle - (std::rand() % 20) * pi / 180;
 
 				//ballSound.play();
-				ball->ball.setPosition(player->position.x + ball->radius + player->dimensions.x / 2 + 0.1f, ball->position.y);
+				//ball->ball.setPosition(enemy->paddle.getPosition().x - ball->radius - enemy->dimensions.x / 2 - 0.5f, ball->ball.getPosition().y);
 			}
+			//if (ball->ball.getPosition().x - ball->radius < player->paddle.getPosition().x + player->dimensions.x / 2 &&
+			//	ball->ball.getPosition().x - ball->radius > player->paddle.getPosition().x &&
+			//	ball->ball.getPosition().y + ball->radius >= player->paddle.getPosition().y - player->dimensions.y / 2 &&
+			//	ball->ball.getPosition().y - ball->radius <= player->paddle.getPosition().y + player->dimensions.y / 2)
+			//{
+			//	if (ball->ball.getPosition().y > player->paddle.getPosition().y)
+			//		ball->ballAngle = pi - ball->ballAngle + (std::rand() % 20) * pi / 180;
+			//	else
+			//		ball->ballAngle = pi - ball->ballAngle - (std::rand() % 20) * pi / 180;
+
+			//	//ballSound.play();
+			//	ball->ball.setPosition(player->paddle.getPosition().x + ball->radius + player->dimensions.x / 2 + 0.1f, ball->ball.getPosition().y);
+			//}
 
 			// Right Paddle
-			if (ball->position.x + ball->radius > enemy->position.x - enemy->dimensions.x / 2 &&
-				ball->position.x + ball->radius < enemy->position.x &&
-				ball->position.y + ball->radius >= enemy->position.y - enemy->dimensions.y / 2 &&
-				ball->position.y - ball->radius <= enemy->position.y + enemy->dimensions.y / 2)
-			{
-				if (ball->position.y > enemy->position.y)
-					ball->ballAngle = pi - ball->ballAngle + (std::rand() % 20) * pi / 180;
-				else
-					ball->ballAngle = pi - ball->ballAngle - (std::rand() % 20) * pi / 180;
+			//if (ball->ball.getPosition().x + ball->radius > enemy->paddle.getPosition().x - enemy->dimensions.x / 2 &&
+			//	ball->ball.getPosition().x + ball->radius < enemy->paddle.getPosition().x &&
+			//	ball->ball.getPosition().y + ball->radius >= enemy->paddle.getPosition().y - enemy->dimensions.y / 2 &&
+			//	ball->ball.getPosition().y - ball->radius <= enemy->paddle.getPosition().y + enemy->dimensions.y / 2)
+			//{
+			//	if (ball->ball.getPosition().y > enemy->paddle.getPosition().y)
+			//		ball->ballAngle = pi - ball->ballAngle + (std::rand() % 20) * pi / 180;
+			//	else
+			//		ball->ballAngle = pi - ball->ballAngle - (std::rand() % 20) * pi / 180;
 
-				//ballSound.play();
-				ball->ball.setPosition(enemy->position.x - ball->radius - enemy->dimensions.x / 2 - 0.1f, ball->position.y);
-			}
+			//	//ballSound.play();
+			//	ball->ball.setPosition(enemy->paddle.getPosition().x - ball->radius - enemy->dimensions.x / 2 - 0.1f, ball->ball.getPosition().y);
+			//}
 		}
 
 
